cd93efc7b69077a8ae71e82e7194313d655c8a73
Lancelot Blanchard <lancelotblanchard@hotmail.fr>
2024-08-28 08:23:39 -0400

    Fix kick drum conditioning in midi-preprocess

--- train/midi-preprocess.py
+++ train/midi-preprocess.py
@@ -1,6 +1,7 @@
 import traceback
 from argparse import ArgumentParser
 from concurrent.futures import ProcessPoolExecutor
+from functools import partial
 from glob import glob
 
 from tqdm import tqdm
@@ -8,7 +9,7 @@ from tqdm import tqdm
 from anticipation.convert import midi_to_compound
 from anticipation.config import PREPROC_WORKERS, TIME_RESOLUTION
 
-def addKickDrum(tokens):
+def addKickDrumToCompound(tokens):
     it = iter(tokens)
 
     BEAT_LENGTH = TIME_RESOLUTION // 2
@@ -36,7 +37,7 @@ def convert_midi(filename, addKickDrum=False, debug=False):
     try:
         tokens = midi_to_compound(filename, debug=debug)
         if addKickDrum:
-            tokens = addKickDrum(tokens)
+            tokens = addKickDrumToCompound(tokens)
     except Exception:
         if debug:
             print('Failed to process: ', filename)
@@ -53,10 +54,12 @@ def convert_midi(filename, addKickDrum=False, debug=False):
 def main(args):
     filenames = glob(args.dir + '/**/*.mid', recursive=True) \
             + glob(args.dir + '/**/*.midi', recursive=True)
+    
+    convert_midi_partial = partial(convert_midi, addKickDrum=args.add_kick_drum)
 
     print(f'Preprocessing {len(filenames)} files with {PREPROC_WORKERS} workers')
     with ProcessPoolExecutor(max_workers=PREPROC_WORKERS) as executor:
-        results = list(tqdm(executor.map(lambda x: convert_midi(x, addKickDrum=args.add_kick_drum), filenames), desc='Preprocess', total=len(filenames)))
+        results = list(tqdm(executor.map(convert_midi_partial, filenames), desc='Preprocess', total=len(filenames)))
 
     discards = round(100*sum(results)/float(len(filenames)),2)
     print(f'Successfully processed {len(filenames) - sum(results)} files (discarded {discards}%)')
