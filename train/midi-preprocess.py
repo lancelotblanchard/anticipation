8c93d2a47d42105076363243cae7ba39229784c6
Lancelot Blanchard <lancelotblanchard@hotmail.fr>
2024-09-08 15:31:46 -0400

    Add drums instead of just kick

--- train/midi-preprocess.py
+++ train/midi-preprocess.py
@@ -9,19 +9,26 @@ from tqdm import tqdm
 from anticipation.convert import midi_to_compound
 from anticipation.config import PREPROC_WORKERS, TIME_RESOLUTION
 
-def addKickDrumToCompound(tokens):
+def addDrumToCompound(tokens):
     it = iter(tokens)
 
     BEAT_LENGTH = TIME_RESOLUTION // 2
+    BAR_LENGTH = BEAT_LENGTH * 4
 
     newTokens = []
     nextBeat = 0
 
     for (time_in_ticks,duration,note,instrument,velocity) in zip(it,it,it,it,it):
         if time_in_ticks >= nextBeat:
+            if nextBeat % BAR_LENGTH == 0:
+                newTokens.append(nextBeat)
+                newTokens.append(BAR_LENGTH-1)
+                newTokens.append(36)
+                newTokens.append(128)
+                newTokens.append(100)
             newTokens.append(nextBeat)
             newTokens.append(BEAT_LENGTH-1)
-            newTokens.append(36)
+            newTokens.append(42)
             newTokens.append(128)
             newTokens.append(100)
             nextBeat += BEAT_LENGTH
@@ -33,11 +40,11 @@ def addKickDrumToCompound(tokens):
 
     return newTokens
 
-def convert_midi(filename, addKickDrum=False, debug=False):
+def convert_midi(filename, addDrum=False, debug=False):
     try:
         tokens = midi_to_compound(filename, debug=debug)
-        if addKickDrum:
-            tokens = addKickDrumToCompound(tokens)
+        if addDrum:
+            tokens = addDrumToCompound(tokens)
     except Exception:
         if debug:
             print('Failed to process: ', filename)
@@ -55,7 +62,7 @@ def main(args):
     filenames = glob(args.dir + '/**/*.mid', recursive=True) \
             + glob(args.dir + '/**/*.midi', recursive=True)
     
-    convert_midi_partial = partial(convert_midi, addKickDrum=args.add_kick_drum)
+    convert_midi_partial = partial(convert_midi, addDrum=args.add_drum)
 
     print(f'Preprocessing {len(filenames)} files with {PREPROC_WORKERS} workers')
     with ProcessPoolExecutor(max_workers=PREPROC_WORKERS) as executor:
@@ -67,5 +74,5 @@ def main(args):
 if __name__ == '__main__':
     parser = ArgumentParser(description='prepares a MIDI dataset')
     parser.add_argument('dir', help='directory containing .mid files for training')
-    parser.add_argument('--add-kick-drum', help='Add a kick drum underneath the files', action="store_true")
+    parser.add_argument('--add-drum', help='Add a drum track underneath the files', action="store_true")
     main(parser.parse_args())
